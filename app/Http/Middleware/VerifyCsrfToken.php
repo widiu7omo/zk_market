3d2c1fd6c7d6e1344336adf036d72a008d58465d
Danar Widi <widiu7omo@gmail.com>
2020-11-06 18:32:58 +0800

    Exception for webhooks route

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,8 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        '/webhooks/ovo',
+        '/webhooks/linkaja',
+        '/webhooks/qris',
     ];
 }
