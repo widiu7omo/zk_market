2df90434ba02bb4a87b9d2c61aacdbf675a47900
Danar Widi <widiu7omo@gmail.com>
2020-11-10 11:38:53 +0800

    Fix View Pegawai

--- resources/views/admin/pegawai/edit.blade.php
+++ resources/views/admin/pegawai/edit.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="px-4 md:px-10 mx-auto w-full h-full -mt-24">
+    <div class="px-4 md:px-10 mx-auto w-full h-full -mt-24">
         <div class="flex flex-wrap">
             <div class="w-full xl:mx-auto xl:w-full mb-16 px-4 h-full">
-                <div class="relative flex flex-col min-w-0 break-words h-full w-full mb-6 shadow-lg rounded-lg bg-gray-100 w-full xl:max-w-screen-lg xl:mx-auto">
+                <div
+                    class="relative flex flex-col min-w-0 break-words h-full w-full mb-6 shadow-lg rounded-lg bg-gray-100 w-full xl:max-w-screen-lg xl:mx-auto">
                     <div class="rounded-t mb-0 px-4 py-3 bg-transparent">
                         <div class="flex flex-wrap items-center">
                             <div class="relative w-full max-w-full flex-grow flex-1">
@@ -26,12 +27,13 @@
                             @endforeach
                         </ul>
                     @endif
-                    <form method="POST" action="{{ url('/admin/pegawai/' . $pegawai->id) }}" accept-charset="UTF-8" class="form-horizontal" enctype="multipart/form-data">
-                                                {{ method_field('PATCH') }}
-                                                {{ csrf_field() }}
+                    <form method="POST" action="{{ url('/admin/pegawai/' . $pegawai->id) }}" accept-charset="UTF-8"
+                          class="form-horizontal" enctype="multipart/form-data">
+                        {{ method_field('PATCH') }}
+                        {{ csrf_field() }}
                         <div class="flex flex-wrap p-3 justify-start">
 
-                                @include ('admin.pegawai.form', ['formMode' => 'edit'])
+                            @include ('admin.pegawai.form', ['formMode' => 'edit'])
 
                         </div>
                     </form>
--- resources/views/admin/pegawai/show.blade.php
+++ resources/views/admin/pegawai/show.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="px-4 md:px-10 mx-auto w-full h-full -mt-24">
+    <div class="px-4 md:px-10 mx-auto w-full h-full -mt-24">
         <div class="flex flex-wrap">
             <div class="w-full xl:mx-auto xl:w-full mb-16 px-4 h-full">
-                <div class="relative flex flex-col min-w-0 break-words h-full w-full mb-6 shadow-lg rounded-lg bg-gray-100 xl:max-w-screen-lg xl:mx-auto">
+                <div
+                    class="relative flex flex-col min-w-0 break-words h-full w-full mb-6 shadow-lg rounded-lg bg-gray-100 xl:max-w-screen-lg xl:mx-auto">
                     <div class="rounded-t mb-0 px-4 py-3 bg-transparent">
                         <div class="flex flex-wrap items-center">
                             <div class="relative w-full max-w-full flex-grow flex-1">
@@ -19,10 +20,15 @@
                                class="uppercase tracking-wide text-sm py-3 px-3 bg-red-500 hover:bg-red-400 shadow-lg rounded-lg text-white font-bold mr-2">Kembali</a>
                             <a href="{{ url('/admin/pegawai/' . $pegawai->id . '/edit') }}"
                                class="uppercase tracking-wide text-sm py-3 px-3 bg-brown-lighter hover:bg-brown-dark shadow-lg rounded-lg text-white font-bold mr-2">Edit</a>
-                            <form method="POST" action="{{ url('admin/pegawai' . '/' . $pegawai->id) }}" accept-charset="UTF-8" class="m-0 p-0">
-                               {{ method_field('DELETE') }}
-                               {{ csrf_field() }}
-                               <button type="submit" class="uppercase tracking-wide text-sm py-3 px-3 bg-brown-lighter hover:bg-brown-dark shadow-lg rounded-lg text-white font-bold" title="Delete Pegawai" onclick="return confirmModal(this)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
+                            <form method="POST" action="{{ url('admin/pegawai' . '/' . $pegawai->id) }}"
+                                  accept-charset="UTF-8" class="m-0 p-0">
+                                {{ method_field('DELETE') }}
+                                {{ csrf_field() }}
+                                <button type="submit"
+                                        class="uppercase tracking-wide text-sm py-3 px-3 bg-brown-lighter hover:bg-brown-dark shadow-lg rounded-lg text-white font-bold"
+                                        title="Delete Pegawai" onclick="return confirmModal(this)"><i
+                                        class="fa fa-trash-o" aria-hidden="true"></i> Delete
+                                </button>
                             </form>
                         </div>
                     </div>
@@ -32,7 +38,18 @@
                                 <th class="w-3/12 border-r-2 text-left">ID#</th>
                                 <td class="px-4 py-2">{{ $pegawai->id }}</td>
                             </tr>
-                            <tr><th class="w-3/12 border-r-2 font-bold tracking-wider uppercase"> Nama </th><td class="px-4 py-2 leading-snug"> {{ $pegawai->nama }} </td></tr><tr><th class="w-3/12 border-r-2 font-bold tracking-wider uppercase"> Jenis Kelamin </th><td class="px-4 py-2 leading-snug"> {{ $pegawai->jenis_kelamin }} </td></tr><tr><th class="w-3/12 border-r-2 font-bold tracking-wider uppercase"> Nohp </th><td class="px-4 py-2 leading-snug"> {{ $pegawai->nohp }} </td></tr>
+                            <tr>
+                                <th class="w-3/12 border-r-2 font-bold tracking-wider text-left uppercase"> Nama</th>
+                                <td class="px-4 py-2 leading-snug"> {{ $pegawai->nama }} </td>
+                            </tr>
+                            <tr>
+                                <th class="w-3/12 border-r-2 font-bold tracking-wider text-left uppercase whitespace-no-wrap"> Jenis Kelamin</th>
+                                <td class="px-4 py-2 leading-snug"> {{ $pegawai->jenis_kelamin }} </td>
+                            </tr>
+                            <tr>
+                                <th class="w-3/12 border-r-2 font-bold text-left tracking-wider uppercase whitespace-no-wrap"> Nohp</th>
+                                <td class="px-4 py-2 leading-snug"> {{ $pegawai->nohp }} </td>
+                            </tr>
                         </table>
                     </div>
                 </div>
