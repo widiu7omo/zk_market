56b82cab382996e46bdf236072b1edf1adf60407
Danar Widi <widiu7omo@gmail.com>
2020-11-11 09:56:04 +0800

    Webhooks for ovo

--- app/Http/Controllers/WebhooksController.php
+++ app/Http/Controllers/WebhooksController.php
@@ -2,13 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pembayaran;
+use App\Models\Pesanan;
+use App\Models\StatusBayar;
 use Illuminate\Http\Request;
 
 class WebhooksController extends Controller
 {
-    public function ovo()
+    public function ovo(Request $request, Pembayaran $pembayaran)
     {
-        return response()->json(['status' => 'success']);
+        $response_xendit = json_decode($request->getContent());
+        $dataUpdatePembayaran = [
+            'status_pembayaran' => $response_xendit->status,
+        ];
+        if ($pembayaran->whereExternalId($response_xendit->external_id)->update($dataUpdatePembayaran)) {
+            $dataPembayaran = $pembayaran->whereExternalId($response_xendit->external_id)->first();
+            if ($response_xendit->status === 'COMPLETED') {
+                Pesanan::whereId($dataPembayaran->pesanan_id)->update(['status_bayar_id' => StatusBayar::whereStatusBayar('sudah bayar')->first()->id]);
+            }
+        }
+        return response()->json($response_xendit);
     }
 
     public function linkaja()
